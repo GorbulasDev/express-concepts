3c66721f8662466a0efe109f537c31efafaa9a4c
Mattp <matt@matt.com>
2022-01-15 00:18:27 -0600

    Update packages

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const routes = require('./routes/facts.js') // code API uses to respond to requests
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'content')))
 
 let app = express() // create the app
 
@@ -7,7 +10,8 @@ let app = express() // create the app
 app.use('/api', routes)
 
 // handle a generic response if we hit a route that is not defined
-app.use('*', function(req, res, next) {
+app.use(function(err, req, res, next) {
+    console.error('request to', req.originalUrl, 'errored because\n', err)
     res.status(404).send('Route not found!')
 })
 
